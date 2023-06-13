c5adaecc23148a034671ce423953f0f3689f5bd7
Ilja Slobodianik <slobodianik.ilja@gmail.com>
2023-06-13 20:43:20 +0300

    Add project files.

---
+++ Array/MyArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTask
+{
+    internal class MyArray
+    {
+        private int[] _array;
+        private int _length;
+        public MyArray()
+        {
+            _length = 0;
+            _array = new int[0];
+        }
+        public void Add(int value)
+        {
+            _length++;
+            int[] tmp = new int[_length];
+            for(int i = 0; i < _length-1; i++)
+            {
+                tmp[i] = _array[i];
+            }
+            tmp[_length-1] = value;
+            _array = tmp;
+        }
+        public void Print()
+        {
+            foreach(int value in _array)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Array/Program.cs
@@ -0,0 +1,9 @@
+﻿using ArrayTask;
+
+MyArray array = new();
+array.Add(1);
+array.Add(2);
+array.Add(3);
+array.Add(4);
+array.Add(5);
+array.Print();
