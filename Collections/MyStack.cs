a7e2060b4fe1a775b3f3fb97fd4e4e5c0828bced
Ilja Slobodianik <slobodianik.ilja@gmail.com>
2023-06-30 16:09:46 +0300

    Started doing MyStack class: added variables _length, _top and _stack, properties Length and Top, and a constructor.

---
+++ Collections/MyStack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class MyStack<T>
+    {
+        private int _length;
+        private T _top;
+        private T[] _stack;
+
+        public int Length => _length;
+        public T Top => _top;
+        public MyStack()
+        {
+            _length = 0;
+            _stack = new T[_length];
+        }
+    }
+}
