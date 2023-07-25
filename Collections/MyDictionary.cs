35a2e688e5b9fef183c3e09301ae4265d36d870d
Ilja Slobodianik <slobodianik.ilja@gmail.com>
2023-07-25 18:52:00 +0300

    Added MyDictionary

--- Collections/MyDictionary.cs
+++ Collections/MyDictionary.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,93 @@ using System.Threading.Tasks;
 
 namespace Collections
 {
-    internal class MyDictionary
+    public class MyDictionary<TKey, TValue> :IEnumerable<KeyValuePair<TKey, TValue>>
     {
+
+        private List<TKey> _keys;
+        private List<TValue> _values;
+
+        public int Count => _keys.Count;
+        public TValue this[TKey key]
+        {
+            get
+            {
+                int index = _keys.IndexOf(key);
+                if (index >= 0)
+                {
+                    return _values[index];
+                }
+                throw new KeyNotFoundException();
+            }
+            set 
+            {
+                int index = _keys.IndexOf(key);
+                if (index >= 0)
+                {
+                    _values[index] = value;
+                }
+                else
+                    Add(key, value);
+            }
+        }
+
+        public MyDictionary() 
+        {
+            _keys = new List<TKey>();
+            _values = new List<TValue>();
+        }
+
+        public void Add(TKey key, TValue value) 
+        {
+            if(_keys.Contains(key)) 
+            {
+                _values[_keys.IndexOf(key)] = value;
+            }
+            else
+            {
+                _keys.Add(key);
+                _values.Add(value);
+            }
+        }
+        public void Clear() 
+        {
+            _keys.Clear();
+            _values.Clear();
+        }
+        
+        public bool Remove(TKey key) 
+        {
+            int index = _keys.IndexOf(key);
+            if(index >= 0) 
+            {
+                _keys.RemoveAt(index);
+                _values.RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            int index = _keys.IndexOf(key);
+            if (index >= 0)
+            {
+                value = _values[index];
+                return true;
+            }
+            value = default(TValue);
+            return false;
+        }
+        public bool ContainsKey(TKey key) => _keys.Contains(key);
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            for(int i = 0; i < _keys.Count; i++)
+            {
+                yield return new KeyValuePair<TKey, TValue>(_keys[i], _values[i]);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
     }
 }
